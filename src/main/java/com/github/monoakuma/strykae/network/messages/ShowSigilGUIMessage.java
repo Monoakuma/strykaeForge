9bbdb51444391a7bf772a7803a6bc7b0326d1fc8
monoakuma <realrobodwarf2@gmail.com>
2022-07-20 23:44:28 -0700

    fixing multiplayer networking

--- src/main/java/com/github/monoakuma/strykae/network/messages/ShowSigilGUIMessage.java
+++ src/main/java/com/github/monoakuma/strykae/network/messages/ShowSigilGUIMessage.java
@@ -2,6 +2,7 @@ package com.github.monoakuma.strykae.network.messages;
 
 import com.github.monoakuma.strykae.Strykae;
 import com.github.monoakuma.strykae.core.ICasterCap;
+import com.github.monoakuma.strykae.network.NetworkHelper;
 import com.github.monoakuma.strykae.objects.items.gui.GuiSigilTablet;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.client.Minecraft;
@@ -11,6 +12,8 @@ import net.minecraft.util.IThreadListener;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import static com.github.monoakuma.strykae.core.CasterCap.CDATA_CAPABILITY;
 import static com.github.monoakuma.strykae.core.CasterCap.getCaster;
@@ -25,11 +28,15 @@ public class ShowSigilGUIMessage implements IMessage {
     }
     public static class Handler implements IMessageHandler<ShowSigilGUIMessage,IMessage> {
         @Override
+        @SideOnly(Side.CLIENT)
         public IMessage onMessage(ShowSigilGUIMessage message, MessageContext ctx) {
             IThreadListener mainThread = Minecraft.getMinecraft();
-            EntityPlayer player = Minecraft.getMinecraft().player;
-            mainThread.addScheduledTask(() -> {
-                player.openGui(Strykae.INSTANCE, 1, player.world, 0, 0, 0);
+            mainThread.addScheduledTask(new Runnable() {
+                @Override
+                public void run() {
+                    EntityPlayer player = NetworkHelper.getSidedPlayer(ctx);
+                    player.openGui(Strykae.INSTANCE, 1, player.world, 0, 0, 0);
+                }
             });
             return null;
         }
